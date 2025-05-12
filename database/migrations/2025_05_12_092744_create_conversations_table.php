c4f00c39e7e8fb0efe8bfe7cc4ab559d5b93e328
Nawel <benbachirnawel@gmail.com>
2025-05-12 11:32:43 +0200

    ajout table conversation2

---
+++ database/migrations/2025_05_12_092744_create_conversations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->id();
+            $table->text('titre');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->foreignId('createur_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('interlocuteur1_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('interlocuteur2_id')->constrained('users')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('conversations');
+    }
+};
