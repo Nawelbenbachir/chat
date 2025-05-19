a9da2c09ff5533e18cfb49aa5a0c9b928871a415
Nawel <benbachirnawel@gmail.com>
2025-05-19 11:03:17 +0200

    ajout dans table user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('prenom')->nullable();
             $table->string('pseudo') -> unique()->nullable();
-            $table->string('telephone')->nullable();
+            $table->string('telephone,20')->nullable();
             $table->string('adresse')->nullable();
-            $table->string('ville')->nullable();
-            $table->string('code_postal')->nullable();
+            $table->string('ville,80')->nullable();
+            $table->string('code_postal,12')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
